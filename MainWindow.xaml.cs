a1644d5f280ad7061ee03d8a1174d92b1d479d64
Nandhini S <150906209+Nandhini-1600@users.noreply.github.com>
2024-02-02 15:12:24 +0530

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,13 +13,45 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfAssigment {
+namespace WpfApp6 {
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
    public partial class MainWindow : Window {
       public MainWindow () {
          InitializeComponent ();
+      }
+
+      private void Button_Click (object sender, RoutedEventArgs e) {
+         if (state.SelectedItem == null || city.SelectedItems == null) {
+            MessageBox.Show ("Select all the fields");
+         } else {
+            string State = (state.SelectedItem as ComboBoxItem).Content.ToString ();
+            string City = city.SelectedItem as string;
+            MessageBox.Show ($"State : {State}\n City : {City}", "Selected Location");
+         }
+      }
+
+      private void state_SelectionChanged (object sender, SelectionChangedEventArgs e) {
+         city.Items.Clear ();
+         AddCity ();
+      }
+
+      private void AddCity () {
+         switch ((state.SelectedItem as ComboBoxItem).Content.ToString ()) {
+            case "Tamilnadu":
+               city.Items.Add ("Kanniyakumari");
+               city.Items.Add ("Chennai");
+               city.Items.Add ("Madurai");
+               city.Items.Add ("Trichy");
+               break;
+            case "Kerala":
+               city.Items.Add ("Trissur");
+               city.Items.Add ("Aazhapuzha");
+               city.Items.Add ("Ernakulam");
+               city.Items.Add ("Paalakad");
+               break;
+         }
       }
    }
 }
